57c866ed9b3c599dea542708e83d88ab0e08bdb0
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-07-22 12:02:52 +0530

    Update <code> block css

--- src/lib/MarkdownRenderer.tsx
+++ src/lib/MarkdownRenderer.tsx
@@ -3,11 +3,10 @@ import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import rehypeHighlight from 'rehype-highlight';
 import remarkMath from 'remark-math';
-import { ReactNode } from 'react';
 
 export const MarkdownRender = ({ children }: { children: string }) => {
   return (
-    <div className='prose prose-neutral dark:prose-invert mt-5 max-w-3xl mx-auto pb-6'>
+    <div className='prose prose-neutral dark:prose-invert prose-code:before:content-none prose-code:after:content-none mt-5 max-w-3xl mx-auto pb-6'>
       <Markdown
         remarkPlugins={[remarkGfm, remarkMath]}
         rehypePlugins={[rehypeRaw, rehypeHighlight]}
